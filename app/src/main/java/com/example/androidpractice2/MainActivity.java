779b932d26aa58f315716fe4e1d23aab3689844f
yzq <2238470411@qq.com>
2019-03-13 23:56:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidpractice2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidpractice2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.practice2_for_linearlayout);
+    }
+}
